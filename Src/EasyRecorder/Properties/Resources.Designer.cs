d31a617ac53307ca1f5ae765cdcf403a8eb15cdc
scanhand <scanhand@gmail.com>
2021-05-15 18:41:29 +0900

    Added a refresh icon and updated reset menu item

--- Src/EasyRecorder/Properties/Resources.Designer.cs
+++ Src/EasyRecorder/Properties/Resources.Designer.cs
@@ -293,6 +293,16 @@ namespace ESR.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_refresh_64 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_refresh_64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_simplestop_64 {
             get {
                 object obj = ResourceManager.GetObject("icons8_simplestop_64", resourceCulture);
